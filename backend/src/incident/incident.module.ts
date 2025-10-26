d994b2e2dac3849bf99409b4a7b219a860be1661
ScuBix1 <bastian.monnin1@gmail.com>
2025-10-26 22:03:03 +0100

    update modules to link typeorm

--- backend/src/incident/incident.module.ts
+++ backend/src/incident/incident.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Incident } from './incident.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Incident])],
+})
 export class IncidentModule {}
