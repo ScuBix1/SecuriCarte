79c51a78aecda0743bcf91fa1e438b2b04a5f30e
ScuBix1 <bastian.monnin1@gmail.com>
2026-01-25 18:54:11 +0100

    try to fix jwt cookie & try to hide apiUrl on frontend

--- frontend/src/app/services/incident.service.ts
+++ frontend/src/app/services/incident.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
+import { environment } from '../../../environments/environment';
 import { Incident, UpdateIncident } from '../models/incident.model';
 
 interface CreateIncidentDto {
@@ -20,27 +21,34 @@ interface DeleteIncidentDto {
 
 @Injectable({ providedIn: 'root' })
 export class IncidentService {
-  private readonly API_URL = 'https://securicarte.onrender.com';
+  private readonly API_URL = environment.apiUrl;
   private markerDeletedSource = new Subject<number>();
   markerDeleted$ = this.markerDeletedSource.asObservable();
 
   constructor(private http: HttpClient) {}
 
   createIncident(dto: CreateIncidentDto): Observable<CreateIncidentDto> {
-    return this.http.post<CreateIncidentDto>(`${this.API_URL}/incident`, dto);
+    return this.http.post<CreateIncidentDto>(`${this.API_URL}/incident`, dto, {
+      withCredentials: true,
+    });
   }
 
   getAllIncidents(): Observable<Incident[]> {
-    return this.http.get<Incident[]>(`${this.API_URL}/incident/all`);
+    return this.http.get<Incident[]>(`${this.API_URL}/incident/all`, {
+      withCredentials: true,
+    });
   }
 
   updateIncident(dto: UpdateIncident): Observable<UpdateIncident> {
-    return this.http.patch<UpdateIncident>(`${this.API_URL}/incident`, dto);
+    return this.http.patch<UpdateIncident>(`${this.API_URL}/incident`, dto, {
+      withCredentials: true,
+    });
   }
 
   deleteIncident(dto: DeleteIncidentDto): Observable<DeleteIncidentDto> {
     return this.http.delete<DeleteIncidentDto>(`${this.API_URL}/incident`, {
       body: dto,
+      withCredentials: true,
     });
   }
 
